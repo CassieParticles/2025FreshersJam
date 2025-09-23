d6073953b9393c53f0d1ee748bbc27b97e48a835
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-09-23 22:56:35 +0100

    Flies clear themselves once they go off screen

--- Assets/Code/Game/Bullets/Flies/FlyMovement.cs
+++ Assets/Code/Game/Bullets/Flies/FlyMovement.cs
@@ -15,6 +15,14 @@ public class FlyMovement : ABulletMovement
     public new void FixedUpdate()
     {
         base.FixedUpdate();
+
+        Vector2 screenPos = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Bullet is off screen
+        if(screenPos.x < 0 || screenPos.x > 1 || screenPos.y < 0 || screenPos.y > 1)
+        {
+            ClearBullet();
+        }
     }
 }
 
