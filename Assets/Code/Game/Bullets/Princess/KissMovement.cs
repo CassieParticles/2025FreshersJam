c28022826eee370894b6bc75480080008462661d
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-09-25 13:08:32 +0100

    Kisses despawn once off screen

--- Assets/Code/Game/Bullets/Princess/KissMovement.cs
+++ Assets/Code/Game/Bullets/Princess/KissMovement.cs
@@ -7,4 +7,16 @@ public class KissMovement : ABulletMovement, IDamagePlayer
     {
         return kissDamage;
     }
+
+    private new void FixedUpdate()
+    {
+        base.FixedUpdate();
+        Vector2 screenPos = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Bullet is off screen
+        if (screenPos.x < 0 || screenPos.x > 1 || screenPos.y < 0 || screenPos.y > 1)
+        {
+            ClearBullet();
+        }
+    }
 }
