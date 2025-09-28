0fc18dae82ce0e018853226ce1e10bea6aff1152
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-09-28 14:58:20 +0100

    Added return to menu button press to game

--- Assets/Code/Game/Frog/ReturnToMenu.cs
+++ Assets/Code/Game/Frog/ReturnToMenu.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ReturnToMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputActionAsset inputAction;
+    [SerializeField] GameEvent returnToMenu;
+
+    InputAction input;
+
+    private void Awake()
     {
-        
+        input = inputAction.FindAction("Pause");
+
+        input.performed += Return;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Return(InputAction.CallbackContext callback)
     {
-        
+        returnToMenu.Notify();
     }
 }
